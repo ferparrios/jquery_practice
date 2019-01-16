8b582d2030d56705ff77851e7075c0fe5513ce38
Fernando Paredes <fernando.paredes@sonr.digital>
2019-01-15 22:32:26 -0500

    primer commit

---
+++ js/codigo.js
@@ -0,0 +1,13 @@
+
+/*var caja = document.querySelector("#caja");
+caja.addEventListener("click", cambiarColor);
+
+function cambiarColor(){
+    caja.style.background = "red";
+}*/
+
+$("#caja").click(function(){
+    $("#caja").css({"background":"red", 
+    "width":"400px",
+    "heigth":"100px"});
+});
